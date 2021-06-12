94feeb361054e1ca96f3264a0e5ebc6265b53da0
Alex Smits <alexsmits247@gmail.com>
2021-06-12 13:49:05 +1000

    LMB aims player

--- GMTK21_Unity/Assets/Alex/IsometricCharacterController.cs
+++ GMTK21_Unity/Assets/Alex/IsometricCharacterController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class IsometricCharacterController : MonoBehaviour
 {
@@ -16,6 +18,9 @@ public class IsometricCharacterController : MonoBehaviour
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
     bool kicking;
+
+    Ray ray;
+    RaycastHit hitInfo;
 
     // Start is called before the first frame update
     void Start()
@@ -53,7 +58,7 @@ public class IsometricCharacterController : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if (direction.magnitude >= 0.1f)
+        if (direction.magnitude >= 0.1f && !kicking)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
@@ -77,6 +82,13 @@ public class IsometricCharacterController : MonoBehaviour
 
     void Kick()
     {
-
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            Vector3 direction = (hitInfo.point - transform.position).normalized;
+            Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+        }
     }
 }
