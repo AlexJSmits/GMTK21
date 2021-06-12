8e7e2f66680f06560d45aead40438b33b311686c
Alex Smits <alexsmits247@gmail.com>
2021-06-12 13:27:00 +1000

    New Isometric View

---
+++ GMTK21_Unity/Assets/Alex/BallController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class BallController : MonoBehaviour
+{
+    public float speed = 6;
+
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- GMTK21_Unity/Assets/Alex/IsometricCharacterController.cs
+++ GMTK21_Unity/Assets/Alex/IsometricCharacterController.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class ThirdPersonController : MonoBehaviour
+public class IsometricCharacterController : MonoBehaviour
 {
     private CharacterController controller;
 
     public float speed = 6;
+    public float rotationSpeed = 1;
+
+
     public Transform cam;
+    public Transform camPivot;
 
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
@@ -23,6 +26,8 @@ public class ThirdPersonController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        camPivot.transform.position = transform.position;
+
         PlayerMovement();
 
         if (kicking)
@@ -30,15 +35,16 @@ public class ThirdPersonController : MonoBehaviour
             Kick();
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetMouseButtonDown(0))
         {
             kicking = true;
         }
 
-        if (Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetMouseButtonUp(0))
         {
             kicking = false;
         }
+
     }
 
     void PlayerMovement()
@@ -47,24 +53,26 @@ public class ThirdPersonController : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if (!kicking)
-        {
-            if (direction.magnitude >= 0.1f)
-            {
-                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-                transform.rotation = Quaternion.Euler(0, angle, 0);
-
-                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-                controller.Move(moveDir.normalized * speed * Time.deltaTime);
-            }
-        }
-        else
+        if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0, angle, 0);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            camPivot.transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            camPivot.transform.Rotate(0, -rotationSpeed * Time.deltaTime, 0);
+        }
+
     }
 
     void Kick()
